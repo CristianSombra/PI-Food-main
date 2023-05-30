2867df268e547162d5c05b66ee6639d66bb70b23
CristianSombra <cristiansombra87@gmail.com>
2023-05-30 20:48:58 -0300

    Modelos realizados

--- api/src/models/Diet.js
+++ api/src/models/Diet.js
@@ -1,14 +1,15 @@
 const { DataTypes } = require('sequelize');
+const { v4: uuidv4 } = require('uuid');
 // Exportamos una funcion que define el modelo
 // Luego le injectamos la conexion a sequelize.
 module.exports = (sequelize) => {
   // defino el modelo
   sequelize.define('diet', {
     id: {
-        type: DataTypes.INTEGER,
-        primaryKey: true,
-        autoIncrement: true
-      },
+      type: DataTypes.UUID,
+      primaryKey: true,
+      defaultValue: () => uuidv4(),
+    },
       name: {
         type: DataTypes.STRING,
         allowNull: false
