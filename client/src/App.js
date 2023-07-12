0258fe9b8852f38e835a845008090ca03d5ecff5
CristianSombra <cristiansombra87@gmail.com>
2023-07-12 00:12:41 -0300

    Iniciando el front

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import Home from "./views/home/home";
 
 function App() {
   return (
     <div className="App">
       <h1>Henry Food</h1>
+      <Home></Home>
     </div>
   );
 }
---
+++ client/src/views/home/home.jsx
@@ -0,0 +1,11 @@
+const home = () => {
+    return(
+        <>
+            <h1>Esta es la vista de Home</h1>
+            <h2>Aqui iria otro texto</h2>
+        </>
+    )
+};
+
+
+export default home;
