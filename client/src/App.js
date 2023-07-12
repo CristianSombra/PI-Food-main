27b0af37b64e540ca49349f1d57a0147f941168b
CristianSombra <cristiansombra87@gmail.com>
2023-07-12 16:23:25 -0300

    Front modificaciones

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,21 @@
 import './App.css';
+import { Route, Switch } from "react-router-dom";
 import Home from "./views/home/home";
 import Landing from './views/landing/landing';
+
 
 function App() {
   return (
     <div className="App">
       <h1>Henry Food</h1>
-      <Home></Home>
-      <Landing></Landing>
+      <Route exact path = "/">
+          <Landing />
+      </Route>
+
+      <Route path = "/home">
+          <Home />
+      </Route>
+
     </div>
   );
 }
