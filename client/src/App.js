c1c12539dfbc2c7fc6ec6ac28327b7b8ef27e54b
CristianSombra <cristiansombra87@gmail.com>
2023-07-13 01:37:34 -0300

    Modificaciones

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,22 @@
 import './App.css';
-import { Route, Switch } from "react-router-dom";
+import { Route, useLocation, Switch } from "react-router-dom";
 import { Home, Landing, Detail, Form } from "./views";
+import NavBar from './components/navbar/navbar';
 
 
 function App() {
+
+const location = useLocation();
+
   return (
     <div className="App">
+      {location.pathname !=="/" && <NavBar />}
       <h1>Henry Food</h1>
       <Route exact path ="/" component = {Landing} />
+      <Route path = "/home" render= {() => <Home />} />
       <Route exact path ="/detail" component = {Detail} />
       <Route exact path ="/create" component = {Form} />
 
-      <Route path = "/home" render= {() => <Home />} />
     </div>
   );
 }
