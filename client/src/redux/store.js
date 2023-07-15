b4cc36b2d56f9bdabea4bcf55035831e098da179
CristianSombra <cristiansombra87@gmail.com>
2023-07-15 14:15:48 -0300

    Modificaciones Front

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -2,7 +2,7 @@ import { createStore, applyMiddleware, compose } from "redux";
 import rootReducer from "./reducer";
 import thunkMiddleware from "redux-thunk";
 
-const composeEnhancer = window._REDUX_DEVTOOLS_EXTENSION_COMPOSE_ || compose;
+const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 export const store = createStore(
     rootReducer,
