4107f0f7d13cec5951fc293cc2cc9aad70ae98eb
CristianSombra <cristiansombra87@gmail.com>
2023-07-15 22:30:28 -0300

    Modificaciones Front

--- client/src/components/cardscontainer/cardscontainer.jsx
+++ client/src/components/cardscontainer/cardscontainer.jsx
@@ -1,37 +1,11 @@
 import Card from "../card/card"
 import style from "./cardscontainer.module.css";
+import { useSelector } from "react-redux"; 
 
-// const recipes = useSelector(state=>state.recipes);
 
-const cardsContainer = () => {
+const CardsContainer = () => {
     
-    
-    const recipes = [{
-    "id": "96c744a0-dd73-41d4-9d54-d7ffc6e0abab",
-    "name": "chipa",
-    "image": "ruta/de/la/imagen.jpg",
-    "summary": "las originales son las de mandioca",
-    "healthScore": 10,
-    "steps": "comer calentitas",
-    "diets": [
-        {
-            "name": "dairy free"
-        }
-    ]
-    },
-    {
-    "id": "a30e721c-f3c1-493c-859c-a9c2a6e91fbc",
-    "name": "chips de cholocale",
-    "image": "ruta/de/la/imagen.jpg",
-    "summary": "Los mejores son los de Tante Sara",
-    "healthScore": 14,
-    "steps": "Geniales para la merienda",
-    "diets": [
-        {
-            "name": "gluten free"
-        }
-    ]}
-    ];
+    const recipes = useSelector(state => state.recipes);
     
     return(
         <div className={style.container}>
@@ -51,7 +25,7 @@ const cardsContainer = () => {
 };
 
 
-export default cardsContainer;
+export default CardsContainer;
 
 
 
--- client/src/redux/reducer.js
+++ client/src/redux/reducer.js
@@ -1,6 +1,31 @@
 
 const initialState = {
-    recipes: [],
+    recipes: [{
+        "id": "96c744a0-dd73-41d4-9d54-d7ffc6e0abab",
+        "name": "chipa",
+        "image": "ruta/de/la/imagen.jpg",
+        "summary": "las originales son las de mandioca",
+        "healthScore": 10,
+        "steps": "comer calentitas",
+        "diets": [
+            {
+                "name": "dairy free"
+            }
+        ]
+        },
+        {
+        "id": "a30e721c-f3c1-493c-859c-a9c2a6e91fbc",
+        "name": "chips de cholocale",
+        "image": "ruta/de/la/imagen.jpg",
+        "summary": "Los mejores son los de Tante Sara",
+        "healthScore": 14,
+        "steps": "Geniales para la merienda",
+        "diets": [
+            {
+                "name": "gluten free"
+            }
+        ]}
+        ],
 };
 
 
